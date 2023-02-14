144a4f3a75b62e593c4b7903309a1ff0306ac20a
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-14 19:48:24 -0300

    refactor(vite): remove code created by default and unused config

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import ";',
-      },
-    },
-  },
   plugins: [react()],
   resolve: {
     alias: {
