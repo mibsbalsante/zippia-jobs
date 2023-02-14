6ebcfa2fa85ebb222f974b5ea766e8de57317ea6
Melissa Balsante <mibsbalsante@gmail.com>
2023-02-14 20:08:46 -0300

    feat(components): create job card

---
+++ src/components/ListCard/index.jsx
@@ -0,0 +1,71 @@
+import { clearHTML } from "@utl/regex"
+import PropTypes from "prop-types"
+
+import Ribbon from "@cmp/Ribbon"
+
+import styles from "./styles.module.scss"
+
+const ListCard = ({
+  companyName,
+  companyLogo,
+  easyApplyFlag,
+  estimatedSalary,
+  jobDescription,
+  jobLevels,
+  jobTitle,
+  location,
+}) => (
+  <article className={styles.card}>
+    <Ribbon>{easyApplyFlag}</Ribbon>
+
+    <h2 className={styles.title}>{jobTitle}</h2>
+
+    <p className={styles.company}>
+      {companyLogo && (
+        <img className={styles.companyLogo} src={companyLogo} alt="" />
+      )}
+      <span className={styles.companyName}>{companyName}</span>
+      {location && (
+        <>
+          -<span>{location}</span>
+        </>
+      )}
+    </p>
+
+    <p
+      className={styles.shortDescription}
+      dangerouslySetInnerHTML={{ __html: clearHTML(jobDescription) }}
+    ></p>
+
+    <div className={styles.footer}>
+      <p>{estimatedSalary}</p>
+
+      {jobLevels.length > 0 && (
+        <>
+          <span className={styles.footerSeparator}></span>
+
+          {jobLevels.map(level => (
+            <span className={styles.jobLevel} key={level}>
+              {level}
+            </span>
+          ))}
+        </>
+      )}
+
+      <button className={styles.apply}>Apply</button>
+    </div>
+  </article>
+)
+
+ListCard.propTypes = {
+  companyName: PropTypes.string,
+  companyLogo: PropTypes.string,
+  easyApplyFlag: PropTypes.bool,
+  estimatedSalary: PropTypes.string,
+  jobDescription: PropTypes.string,
+  jobLevels: PropTypes.arrayOf(PropTypes.string),
+  jobTitle: PropTypes.string,
+  location: PropTypes.string,
+}
+
+export default ListCard
